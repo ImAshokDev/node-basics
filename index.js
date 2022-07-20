3dd6315d9d00883a4624991501e23b9d5be16038
ashokDev10 <imashokdev@gmail.com>
2022-07-20 18:37:08 +0530

    node js server added

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
-console.log("Hello world!!!.................");
+// console.log("Hello world!!!.................");
 
-const fs = require("fs");
-const _ = require("lodash");
-const yargs = require("yargs");
+// const fs = require("fs");
+// const _ = require("lodash");
+// const yargs = require("yargs");
 
-const notes = require("./notes.js");
+// const notes = require("./notes.js");
 
-// console.log("process", process.argv);
+// // console.log("process", process.argv);
 
-console.log("yargs", yargs.argv);
+// console.log("yargs", yargs.argv);
 
-var argv = yargs.argv;
-var command = argv._[0];
+// var argv = yargs.argv;
+// var command = argv._[0];
 
-if (command === "list") {
-  notes.getAllNotes();
-  console.log("Listed all notes");
-} else if (command === "read") {
-  notes.readNote(argv.title);
-  console.log("Readed all notes");
-} else if (command === "create") {
-  notes.createNote(argv.title, argv.body);
-  console.log("Note Created");
-} else if (command === "remove") {
-  notes.removeNote(argv.title);
-  console.log("Note Removed");
-} else {
-  console.log("Command Not Found");
-}
+// if (command === "list") {
+//   notes.getAllNotes();
+//   console.log("Listed all notes");
+// } else if (command === "read") {
+//   notes.readNote(argv.title);
+//   console.log("Readed all notes");
+// } else if (command === "create") {
+//   notes.createNote(argv.title, argv.body);
+//   console.log("Note Created");
+// } else if (command === "remove") {
+//   notes.removeNote(argv.title);
+//   console.log("Note Removed");
+// } else {
+//   console.log("Command Not Found");
+// }
+
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.listen(3000, console.log("server Started at port 3000"));
