f38538834af0648998af94a011df5c31a99fd1b9
ashokDev10 <imashokdev@gmail.com>
2022-07-20 13:08:13 +0530

    notes added

--- node/index.js
+++ node/index.js
@@ -2,10 +2,11 @@ console.log("Hello world!!!");
 
 const fs = require("fs");
 const os = require("os");
+const notes = require("./notes.js");
 
 const userInfo = os.userInfo();
 
-fs.appendFile("message.txt", `userData: ${userInfo.username}`, (err) => {
+fs.appendFile("message.txt", ` You are ${notes.age} `, (err) => {
   if (err) {
     console.log("error....", err);
   }
---
+++ node/notes.js
@@ -0,0 +1,3 @@
+console.log("notes started");
+
+module.exports.age = 26;
