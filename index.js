093ca3267c49b1f494171388dc6942f78e2d12ea
ashokDev10 <imashokdev@gmail.com>
2022-07-20 14:57:35 +0530

    npm install

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello world!!!");
+
+const _ = require("lodash");
+
+const arr = [1, 2, 3, 2, 5, 4, 3];
+
+console.log(_.isString("abc"));
+console.log(_.isString(2));
+console.log(_.uniq(arr));
