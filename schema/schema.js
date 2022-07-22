27d0ec9ec022d528f47d86787d5b21c2a18b3b33
ashokDev10 <imashokdev@gmail.com>
2022-07-22 16:01:47 +0530

    studentlist added

--- schema/schema.js
+++ schema/schema.js
@@ -4,9 +4,9 @@ const _ = require("lodash");
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 var books = [
-  { name: "Book 1", genre: "genre 1", id: "1" },
-  { name: "Book 2", genre: "genre 2", id: "2" },
-  { name: "Book 3", genre: "genre 3", id: "3" },
+  { name: "Book 1", genre: "genre 1", id: "1", authorId: "1" },
+  { name: "Book 2", genre: "genre 2", id: "2", authorId: "2" },
+  { name: "Book 3", genre: "genre 3", id: "3", authorId: "3" },
 ];
 
 var authors = [
@@ -14,6 +14,30 @@ var authors = [
   { name: "Author 2", age: "26", id: "2" },
   { name: "Author 3", age: "27", id: "3" },
 ];
+
+var studentList = [
+  { id: "1", name: "suriya", class: "12th", rank: "1" },
+  { id: "2", name: "vijay", class: "10th", rank: "3" },
+  { id: "3", name: "ajith", class: "8th", rank: "2" },
+];
+
+const StudentListType = new GraphQLObjectType({
+  name: "StudentList",
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    class: {
+      type: GraphQLString,
+    },
+    rank: {
+      type: GraphQLString,
+    },
+  }),
+});
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -26,6 +50,12 @@ const BookType = new GraphQLObjectType({
     },
     genre: {
       type: GraphQLString,
+    },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId });
+      },
     },
   }),
 });
@@ -48,6 +78,15 @@ const AuthorType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    studentList: {
+      type: StudentListType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // code to get data from db
+        return _.find(studentList, { id: args.id });
+      },
+    },
+
     book: {
       type: BookType,
       args: { id: { type: GraphQLID } },
